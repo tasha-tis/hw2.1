eaf70d931391eb62485d8d41acc6de26a329d448
Tatsiana sys <tasha_tis@mail.ru>
2016-04-03 19:50:40 +0300

    advacnedOperation commit3

--- Base.java
+++ Base.java
@@ -7,4 +7,9 @@ class Base {
 		System.out.println("This is add method for 3 params")
 		return a + b + c;
 	} 
+	
+	public int add(int a, int b, int c, int d){
+		System.out.println("This is add method for 4 params")
+		return a + b + c + d;
+	} 
 }
