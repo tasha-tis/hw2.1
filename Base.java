7a1480dc49ee93c04a0d1f8967a6714d139723d1
Tatsiana sys <tasha_tis@mail.ru>
2016-04-03 20:02:10 +0300

    advancedOperation

---
+++ Base.java
@@ -0,0 +1,15 @@
+class Base {
+	public int add(int a, int b){
+		System.out.println("This is add method.")
+		return a + b;
+	} 
+	public int add(int a, int b, int c){
+		System.out.println("This is add method for 3 params")
+		return a + b + c;
+	} 
+	
+	public int add(int a, int b, int c, int d){
+		System.out.println("This is add method for 4 params")
+		return a + b + c + d;
+	} 
+}
